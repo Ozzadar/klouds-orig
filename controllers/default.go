8c00a2daa01c9e7e3eb785db39a5759cb9e8fb48
= <ozzadar@ozzadar.com>
2015-10-14 15:17:01 +0700

    Added delete app confirmation message. Fixed codiad deploy file. A nasty pusher added a line without testing a few commits back.

--- controllers/default.go
+++ controllers/default.go
@@ -215,6 +215,31 @@ func (this *DeleteController) Get() {
 	}
 
 	appName  := this.Ctx.Input.Param(":appName")
+	this.Data["YesOrNo"]="<h1>Really delete this application? (this process is irreversible):</h1><br> <form action='/deleteApp/" + appName + 
+	"' method='post' id='form1'><button type='submit' " +
+	"form='form1' value='YES'>Yes, do it!</button> </form><br><form action='/user/profile' method='get' id='form2'>" +
+	" <button type='submit' form='form2' value='NO'>I changed my mind! SAVE MY APP!</button></form>"
+
+/*
+	time.Sleep(5 * time.Second)
+	this.Redirect("/user/profile", 302)
+*/
+	m := sess.(map[string]interface{})
+	fmt.Println("username is", m["username"])
+	fmt.Println("logged in at", m["timestamp"])
+}
+
+func (this *DeleteController) Post() {
+	this.activeContent("deleteApp")
+	this.Data["YesOrNo"] = "<h1>Deleting application. Will be redirected in 2 seconds.</h1>"
+		//******** This page requires login
+	sess := this.GetSession("acme")
+	if sess == nil {
+		this.Redirect("/user/login/home", 302)
+		return
+	}
+
+	appName  := this.Ctx.Input.Param(":appName")
 
 	//Create the request
 	url := "http://" + os.Getenv("MARATHON_ENDPOINT")+ "/v2/apps/" + string(appName)
@@ -233,10 +258,9 @@ func (this *DeleteController) Get() {
 	}
 
 	fmt.Println(res.StatusCode)
-/*
-	time.Sleep(5 * time.Second)
+
+	time.Sleep(2 * time.Second)
 	this.Redirect("/user/profile", 302)
-*/
 	m := sess.(map[string]interface{})
 	fmt.Println("username is", m["username"])
 	fmt.Println("logged in at", m["timestamp"])
--- views/deleteApp.tpl
+++ views/deleteApp.tpl
@@ -1,12 +1,10 @@
 	<!-- start content -->
-	<meta http-equiv="refresh" content="2;url=/user/profile" />
 	<div id="content">
 		<div class="post">
 			
 			<div class="entry">
 				<div align='center'>
-					DELETING APPLICATION : {{.ApplicationName}}<br>
-					redirecting back to profile in 2 seconds ...
+				  <br> {{str2html .YesOrNo}}
 				</div><div align='center'>
 					
 			</div></div>
