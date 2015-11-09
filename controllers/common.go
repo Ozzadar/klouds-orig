7d4005e5e713d4156a48529c2c943626e5a4391a
Paul Mauviel <ozzadar@ozzadar.com>
2015-11-10 00:27:02 +0700

    Improved random string generator. Was getting many duplicates. New one works much better.

--- controllers/common.go
+++ controllers/common.go
@@ -263,20 +263,12 @@ const (
     letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
 )
 
-func RandString(n int) string {
-    b := make([]byte, n)
-    // A rand.Int63() generates 63 random bits, enough for letterIdxMax letters!
-    for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
-        if remain == 0 {
-            cache, remain = rand.Int63(), letterIdxMax
-        }
-        if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-            b[i] = letterBytes[idx]
-            i--
-        }
-        cache >>= letterIdxBits
-        remain--
-    }
-
-    return string(b)
+func RandString(strlen int) string {
+	rand.Seed(time.Now().UTC().UnixNano())
+	const chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+	result := make([]byte, strlen)
+	for i := 0; i < strlen; i++ {
+		result[i] = chars[rand.Intn(len(chars))]
+	}
+	return string(result)
 }
