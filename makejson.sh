c8b858456e8833f51f0d057d2cfa78799e6f0987
Paul Mauviel <ozzadar@ozzadar.com>
2015-11-18 15:33:15 +0700

    Wercker?

---
+++ makejson.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+cat > klouds-ui.json <<EOF
+{
+  "id": "/klouds-ui",
+  "cpus": 1,
+  "mem": 256,
+  "instances": 1,
+  "constraints": [["hostname", "UNIQUE"]],
+  "acceptedResourceRoles": ["slave_public"],
+  "container": {
+    "type": "DOCKER",
+    "docker": {
+      "image": "$APP_IMAGE",
+      "network": "BRIDGE",
+      "forcePullImage": true,
+      "portMappings": [
+          {
+              "containerPort": 8080,
+              "hostPort": 80,
+              "protocol": "tcp"
+          }
+      ]
+    }
+  },
+  "healthChecks": [{
+      "protocol": "TCP",
+      "gracePeriodSeconds": 600,
+      "intervalSeconds": 30,
+      "portIndex": 0,
+      "timeoutSeconds": 10,
+      "maxConsecutiveFailures": 2
+  }].
+}
+EOF
