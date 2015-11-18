ee7e21377a6a7f7c2be16f323fb99eaf21ea9fae
Paul Mauviel <ozzadar@ozzadar.com>
2015-11-18 16:04:40 +0700

    more json changes

--- makejson.sh
+++ makejson.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 cat > klouds-ui.json <<EOF
 {
-  "id": "/klouds-ui",
-  "cpus": 1,
-  "mem": 256,
-  "instances": 1,
-  "constraints": [["hostname", "UNIQUE"]],
-  "acceptedResourceRoles": ["slave_public"],
-  "container": {
-    "type": "DOCKER",
-    "docker": {
-      "image": "$APP_IMAGE",
-      "network": "BRIDGE",
-      "forcePullImage": true,
-      "portMappings": [
+    "id":"klouds-ui",
+    "cpus": 0.01,
+    "mem": 50.0,
+    "instances": 1,
+
+    "container": {
+      "type": "DOCKER",
+      "docker": {
+        "image": "$APP_IMAGE",
+        "network": "BRIDGE",
+        "portMappings": [
           {
-              "containerPort": 8080,
-              "protocol": "tcp"
+            "containerPort": 80,
+            "protocol": "tcp"
           }
-      ]
+        ],
+        "privileged": true,
+        "forcePullImage": true
+      }
+    },
+    "env": {
+        
+    },
+    "ports":[10000],
+    "labels": {
+        "HAPROXY_HTTP": "true",
+        "HTTP_PORT_IDX_0_NAME": "klouds-ui"
     }
-  },
-  "healthChecks": [{
-      "protocol": "TCP",
-      "gracePeriodSeconds": 600,
-      "intervalSeconds": 30,
-      "portIndex": 0,
-      "timeoutSeconds": 10,
-      "maxConsecutiveFailures": 2
-  }]
 }
 EOF
